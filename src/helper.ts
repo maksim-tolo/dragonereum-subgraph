9e096c7aed662adda39a4bec67ea57b524fb8dde
Maksim Tolochko <maksim.tolo@gmail.com>
2019-07-20 20:20:01 +0300

    Fix build

--- src/helper.ts
+++ src/helper.ts
@@ -3,19 +3,17 @@ import { User, UserBattlesStat } from '../generated/schema';
 export function initUser(userId: string): User {
   let user = User.load(userId);
 
-  if (user != null) {
-    return user;
+  if (user == null) {
+    let userBattlesStat = new UserBattlesStat(userId);
+
+    userBattlesStat.defeats = 0;
+    userBattlesStat.wins = 0;
+    userBattlesStat.save();
+
+    user = new User(userId);
+    user.battlesStat = userId; // Reference to UserBattlesStat
+    user.save();
   }
 
-  let userBattlesStat = new UserBattlesStat(userId);
-
-  userBattlesStat.defeats = 0;
-  userBattlesStat.wins = 0;
-  userBattlesStat.save();
-
-  user = new User(userId);
-  user.battlesStat = userId; // Reference to UserBattlesStat
-  user.save();
-
-  return user;
+  return user as User;
 }
